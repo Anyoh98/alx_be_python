00d55d590129173fe07b3546d425e8988dab0e72
Anyoh98 <fonachuanyoh@yahoo.com>
2026-03-07 23:36:35 -0500

    This contains additions and subtrations and product of numbers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+#Calculating the sum of number 1 and 2
+Sum = number1 + number2
+
+#Calculating the difference
+Difference = number1 - number2
+
+#Claculating the product
+
+Product = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {Sum}")
+print(f"Subtraction of {number1} and {number2} is {Difference}")
+print(f"Multiplication of {number1} and {number2} is {Product}")
